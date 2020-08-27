0735af309f48297a3f25738b18f634d0464442fc
Jasjot Singh <jasjot784@gmail.com>
2020-08-27 21:30:13 +0530

    Added field and dependency injection

--- app/src/main/java/com/jasjotsingh/hiltbasics/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/hiltbasics/MainActivity.java
@@ -3,12 +3,55 @@ package com.jasjotsingh.hiltbasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
+    //field injection
+    @Inject
+    SomeClass someClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        someClass = new SomeClass();
+        Toast.makeText(this, someClass.doAThing(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, someClass.doSomeOtherThing(), Toast.LENGTH_LONG).show();
     }
 }
+
+ class SomeClass{
+    SomeOtherClass otherClass;
+    @Inject
+     public SomeClass() {
+        //Constructor Injection
+         otherClass = new SomeOtherClass();
+     }
+
+     @Inject
+
+     public String doAThing(){
+         return "Look I did a thing";
+     }
+
+     public String doSomeOtherThing(){
+        return otherClass.doSomeOtherThing();
+     }
+ }
+
+ class SomeOtherClass{
+
+     @Inject
+     public SomeOtherClass() {
+     }
+
+     public String doSomeOtherThing(){
+         return "Look I did some other thing";
+     }
+ }
