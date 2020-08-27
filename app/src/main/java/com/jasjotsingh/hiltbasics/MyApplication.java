4c267457a220a3c52145409b3d9f39b20e5a3cf9
Jasjot Singh <jasjot784@gmail.com>
2020-08-27 18:05:45 +0530

    Added Application class

---
+++ app/src/main/java/com/jasjotsingh/hiltbasics/MyApplication.java
@@ -0,0 +1,9 @@
+package com.jasjotsingh.hiltbasics;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApplication extends Application {
+}
